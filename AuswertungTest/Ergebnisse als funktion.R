1d006447e2b9bade8164e2bf14bfb04519e27d64
Poppi <johannpopp@gmx.net>
2015-04-29 10:08:57 +0200

    Jetzt wird der Freitext auch gut leserlich dargestellt

--- AuswertungTest/Ergebnisse als funktion.R
+++ AuswertungTest/Ergebnisse als funktion.R
@@ -4,39 +4,56 @@
 
 # Johann Popp
 # Erstellt:             2015-03-30
-# Letzte Überarbeitung: 2015-04-01
+# Letzte Überarbeitung: 2015-04-28
 #############################################
 
 
 
 
-###### Eingaben #########
-
-### Wie heißt das SAPV-Team, in dem die Befragung durchgeführt wurde?
-SAPVteam <- "Testdaten"
-### Wer hat die Daten ausgewertet?
-Auswerter <- character()
-### Datendatei
-Eingabe <- "Mitarbeiterinnenbefragung_Dataform_1_15.csv"
-
-Auswertung <- function(daten, team = SAPVteam, auswerter = Auswerter, art = "orig", datEingabe = FALSE, erklAusg = FALSE, erklSynt = FALSE, hist = TRUE, box = TRUE, grid = TRUE, kA = TRUE){
+Auswertung <- function(daten = "", team = "", auswerter = "", art = "", datEingabe = FALSE, erklAusg = FALSE, erklSynt = FALSE, hist = TRUE, box = TRUE, grid = TRUE, kA = TRUE){
   
-  ### Daten einlesen (die in EpiData mit ";" als Trennzeichen als csv exportiert wurden)
-  if(datEingabe == FALSE){
-    Eingabe <- (file.choose())
-    dat <- read.csv2(Eingabe) 
-    dat[dat == 99] <- NA
-    Pf <- strsplit(Eingabe, "/")[[1]]
-    Pfad <- paste(Pf[-length(Pf)], "/", sep = "",  collapse = "")
+  ### Auswahlfunktionen
+  # Team
+  if(team == ""){
+    team <- readline("Welches SAPV-Team wurde befragt? ")
   }
   
-  # Falls die Daten nicht aus einer csv-Datei eingelesen werden sollen, sondern innerhalb von R in eine Datenmatrix eingegeben werden sollen, kann das Argument "datEingabe = TRUE" in die Funktion Ausgwertung eingetragen werden.
+  # Ausgabeart
+  if(art == ""){
+    antwort <- readline("Welche Werte sollen angezeigt werden?\n 1 = Orginalangaben\n 2 = Abweichung von der persönlichen Gesamtbewertung\n 3 = Abweichung vom persönlichen Median\n")
+    if(antwort != 1 & antwort != 2 & antwort != 3) {
+      print("Als Eingabe wird eine Zahl zwischen 1 und 3 erwartet. Die Ausgabeart wurde auf den Wert '1 = Orginalangaben' gesetzt.")
+      antwort <- "1"
+    }
+    art <- c("orig", "abwGesamt", "abwMedian")[as.numeric(antwort)]
+  }
+
+  ### Daten einlesen (die in EpiData mit ; als Trennzeichen als csv exportiert wurden)
+  if(datEingabe == FALSE){
+    if(daten == ""){
+      einlesen <- readline("Sollen die Daten aus einer csv-Datei (mit ; als Trennzeichen) eingelesen oder neu eingegeben werden?\n 1 = einlesen\n 2 = neu eingeben")
+      if(einlesen != 1 & einlesen != 2){
+        cat("Als Eingabe wird eine Zahl zwischen 1 und 2 erwartet. \nEs wird jetzt davon ausgegangen, dass Sie Daten aus einer csv-Datei einlesen möchten")
+        einlesen <- 1
+      }
+      if(einlesen == 1){
+        Eingabe <- (file.choose())
+        dat <- read.csv2(Eingabe, as.is = TRUE) 
+        dat[dat == 99] <- NA        
+      }
+    }
+  }
+  
+  # Falls die Daten nicht aus einer csv-Datei eingelesen werden sollen, sondern innerhalb von R in eine Datenmatrix eingegeben werden sollen, kann das Argument "datEingabe = TRUE" in die Funktion Auswertung eingetragen werden.
   # Die Bewertungen werden numerisch kodiert: 1 = sehr zufriedenstellend; 5 = sehr verbesserungswürdig
-  if(datEingabe == TRUE){
+  if(datEingabe == TRUE | einlesen == 2){
     dat <- data.frame(ID = numeric(), QGesamt = numeric(), SympGesamt = numeric(), SympErfass = numeric(), SympDoku = numeric(), SympBehand = numeric(), SympVorbeug = numeric(), SympSchulung = numeric(), SympSchmerz = numeric(), SympLuft = numeric(), SympAngst = numeric(), SympNausea = numeric(), SympObsti = numeric(), SympFatigue = numeric(), SympKogni = numeric(), SympWunde = numeric(), SympAndere = character(), SympAndWert = numeric(), SympAnmerk = character(), SicherGesamt = numeric(), SicherKriseVerm  = numeric(), SicherKriseAuff = numeric(), SicherMedi = numeric(), SicherInfekt = numeric(), SicherVerletz = numeric(), SicherAndere = character(), SicherAndWert = numeric(), SicherAnmerk = character(), AllGesamt = numeric(), AllKrankheit = numeric(), AllPflege = numeric(), AllSozial = numeric(), AllOrga = numeric(), AllSpirit = numeric(), AllKultur = numeric(), AllAnmerk = character(), Freitext = character())
     fix(dat)
-    write.csv2(dat, file.choose(), row.names = FALSE)
-    Pfad <- ""
+    readline("Wälen Sie einen Speicherort.\n
+             Weiter mit beliebiger Taste.")
+    Eingabe <- file.choose()
+    write.csv2(dat, Eingabe, row.names = FALSE)
+    
   }
 
   
@@ -59,7 +76,19 @@ Auswertung <- function(daten, team = SAPVteam, auswerter = Auswerter, art = "ori
   
   
   ### Name der Ausgabedatei erstellen
-  Ausgabe <- paste(Pfad, "Ergebnisse der Mitarbeiterbefragung - ", team, " ", Sys.Date(), ".pdf", sep = "")
+  # Pfad der Eingabedatei extrahieren
+  Pf <- strsplit(Eingabe, "/")[[1]]
+  Pfad <- paste(Pf[-length(Pf)], "/", sep = "",  collapse = "")
+  # Art der Darstellung anhängen
+  Typ <- " "
+  if(art == "abwGesamt"){
+    Typ <- " Abweichung von Gesamt "
+  }
+  if(art == "abwMedian"){
+    Typ <- " Abweichung vom Median "
+  }
+  # Zusammen führen
+  Ausgabe <- paste(Pfad, "Ergebnisse der MitarbeiterInnenbefragung - ", team, Typ, Sys.Date(), ".pdf", sep = "")
   
   ### Einstellungen für die verschiedenen Darstellungsformen
   hoehe <- length(dat[,1])
@@ -118,6 +147,50 @@ Auswertung <- function(daten, team = SAPVteam, auswerter = Auswerter, art = "ori
     }
   }
   
+  # Funktion zur Zeilenaufteilung der Freitexte
+  Zeilen <- function(x, länge = 130, ...){
+    strwrap(
+      unlist(
+        strsplit(
+          paste(unlist(
+            strsplit(x, "-&&-")
+          ), 
+          collapse = "\\n\\n"), 
+          "\\\\n")
+      ), länge
+    )  
+  }
+  
+  # Funktion für zusätzliche Textseiten
+  Textseite <- function(titel, daten, seite, i){
+    lmatr5 <- matrix(c(1,2,3), nrow = 3)
+    layout(lmatr5, heights = lcm(c(2.5,16,0.5)))
+    
+    # Zeile 1
+    plot.new()
+    text(-0.02, 0.2, paste(strwrap(titel, 105), collapse = "\n", sep = ""), adj = c(0,NA), cex = 1.8, font = 2)
+    
+    # Zeile 2
+    plot(10, ylim = c(0,5), xlim = c(0,1), axes = FALSE)
+    text(-0.02, 5,
+         paste(Zeilen(daten)[seq(i*30-29,min(c(i*30, length(Zeilen(daten)))))], collapse = "\n"), adj = c(0, 1), cex = 1.5)
+    
+    # Zeile 3
+    plot.new()
+    lines(c(-0.02,3), c(1,1))
+    text(1.04, 0.1, paste("Seite", seite, "von", GesSeiten), adj = c(1,0))
+    text(-0.02, 0.1, Fussn, adj = c(0,0))
+  }
+  
+  
+  ########## Seitennummern ermitteln #############
+  Seiten <- data.frame(Symp = sum(length(Zeilen(dat$SympAnmerk)) >8, trunc(length(Zeilen(dat$SympAnmerk))/30)), 
+                       Sich = sum(length(Zeilen(dat$SicherAnmerk)) >8, trunc(length(Zeilen(dat$SicherAnmerk))/30)), 
+                       Allg = sum(length(Zeilen(dat$AllAnmerk))>8, trunc(length(Zeilen(dat$AllAnmerk))/30)),
+                       Frei = ceiling(length(Zeilen(dat$Freitext))/30))
+  GesSeiten <- sum(Seiten, 4)
+
+  
   ########## pdf-Dokument erstellen #########
   
   pdf(Ausgabe, paper = "a4r", width = 10.5, height = 7.5)
@@ -154,7 +227,7 @@ Auswertung <- function(daten, team = SAPVteam, auswerter = Auswerter, art = "ori
   
   # Zeile 3
   plot.new()
-  text(0, 0.5, paste(strwrap(paste("Wie schätzt du die Qualität der Arbeit des SAPV-Teams", team, "insgesamt ein?"), 72), collapse = "\n", sep = ""), adj = c(0,NA), cex = 1.5)
+  text(0, 0.5, paste(strwrap(paste("Wie schätzt du die Qualität der Arbeit des SAPV-Teams", team, "insgesamt ein?"), 70), collapse = "\n", sep = ""), adj = c(0,NA), cex = 1.5)
   
   ergebnis(dat$QGesamt, farbe = "#2b8cbe99")
   
@@ -211,7 +284,7 @@ Auswertung <- function(daten, team = SAPVteam, auswerter = Auswerter, art = "ori
   # Copyright
   plot.new()
   lines(c(-0.02,3), c(0.25,0.25))
-  text(1.04, 0.1, "Seite 1 von 5", adj = c(1,0))
+  text(1.04, 0.1, paste("Seite 1 von", GesSeiten), adj = c(1,0))
   text(-0.02, 0.1, Fussn, adj = c(0,0))
   
   
@@ -305,16 +378,28 @@ Auswertung <- function(daten, team = SAPVteam, auswerter = Auswerter, art = "ori
   
   # Zeile 12
   plot(10, ylim = c(0,5), xlim = c(0,1), axes = FALSE)
-  text(-0.02, 5, 
-       paste(strwrap(paste(dat$SympAnmerk[grep('[:alphanum:]', dat$SympAnmerk)], collapse = '  ---  '), 130), collapse = "\n", sep = ""),
-       adj = c(0, 1), cex = 1.5)
+  if(length(Zeilen(dat$SympAnmerk)) < 9){
+    text(-0.02, 5, 
+         paste(Zeilen(dat$SympAnmerk), collapse = "\n"),
+         adj = c(0, 1), cex = 1.5)
+  }
+  else{
+    text(-0.02, 4.8, "Siehe nächste Seite", adj = c(0,NA), cex = 1.5)
+  }
   
   
   # Zeile 13
   plot.new()
   lines(c(-0.02,3), c(1,1))
-  text(1.04, 0.1, "Seite 2 von 5", adj = c(1,0))
+  text(1.04, 0.1, paste("Seite 2 von", GesSeiten), adj = c(1,0))
   text(-0.02, 0.1, Fussn, adj = c(0,0))
+  
+  ###########################
+  # Zwischenseite(n)
+  if(length(Zeilen(dat$SympAnmerk)) > 8){
+    for(i in 1:Seiten$Symp)
+    Textseite(titel = txt[25], daten = dat$SympAnmerk, seite = i + 2, i = i)
+  }
   
   
   #####################
@@ -394,18 +479,28 @@ Auswertung <- function(daten, team = SAPVteam, auswerter = Auswerter, art = "ori
   
   # Zeile 10
   plot(10, ylim = c(0,5), xlim = c(0,1), axes = FALSE)
-  text(-0.02, 5, 
-       paste(strwrap(paste(dat$SicherAnmerk[grep('[:alphanum:]', dat$SicherAnmerk)], collapse = '  ---  '), 130), collapse = "\n", sep = ""),
-       adj = c(0, 1), cex = 1.5)
-  
+  if(length(Zeilen(dat$SicherAnmerk)) < 9){
+    text(-0.02, 5, 
+         paste(Zeilen(dat$SicherAnmerk), collapse = "\n"),
+         adj = c(0, 1), cex = 1.5)
+  }
+  else{
+    text(-0.02, 4.8, "Siehe nächste Seite", adj = c(0,NA), cex = 1.5)
+  }
   
   # Zeile 11
   plot.new()
   lines(c(-0.02,3), c(1,1))
-  text(1.04, 0.1, "Seite 3 von 5", adj = c(1,0))
+  text(1.04, 0.1, paste("Seite", sum(Seiten[1]) + 3, "von", GesSeiten), adj = c(1,0))
   text(-0.02, 0.1, Fussn, adj = c(0,0))
   
   
+  ###########################
+  # Zwischenseite(n)
+  if(length(Zeilen(dat$SicherAnmerk)) > 8){
+    for(i in 1:Seiten$Sich)
+      Textseite(titel = txt[42], daten = dat$SicherAnmerk, seite = i + sum(Seiten[1])+3, i = i)
+  }
   
   #####################
   # Seite 4
@@ -499,43 +594,79 @@ Auswertung <- function(daten, team = SAPVteam, auswerter = Auswerter, art = "ori
   
   # Zeile 10
   plot(10, ylim = c(0,5), xlim = c(0,1), axes = FALSE)
-  text(-0.02, 5, 
-       paste(strwrap(paste(dat$AllAnmerk[grep('[:alphanum:]', dat$AllAnmerk)], collapse = '  ---  '), 130), collapse = "\n", sep = ""),
-       adj = c(0, 1), cex = 1.5)
-  
+  if(length(Zeilen(dat$AllAnmerk)) < 9){
+    text(-0.02, 5, 
+         paste(Zeilen(dat$AllAnmerk), collapse = "\n"),
+         adj = c(0, 1), cex = 1.5)
+  }
+  else{
+    text(-0.02, 4.8, "Siehe nächste Seite", adj = c(0,NA), cex = 1.5)
+  }
   
   # Zeile 11
   plot.new()
   lines(c(-0.02,3), c(1,1))
-  text(1.04, 0.1, "Seite 4 von 5", adj = c(1,0))
+  text(1.04, 0.1, paste("Seite", sum(Seiten[1:2]) + 4,"von", GesSeiten), adj = c(1,0))
   text(-0.02, 0.1, Fussn, adj = c(0,0))
+
   
+  ###########################
+  # Zwischenseite(n)
+  if(length(Zeilen(dat$AllAnmerk)) > 8){
+    for(i in 1:Seiten$Allg){
+      Textseite(titel = txt[57], daten = dat$AllAnmerk, seite = i + sum(Seiten[1:2])+4, i = i)
+    }
+  }
   
   ##################################
   # Seite 5
   
-  lmatr5 <- matrix(c(1,2,3), nrow = 3)
-  layout(lmatr5, heights = lcm(c(2.5,16,0.5)))
+  for(i in 1:Seiten$Frei){
+    Textseite(titel = paste(strwrap(paste("Welche weiteren Qualitätsaspekte sind dir bei der Arbeit des SAPV-Teams", SAPVteam, "wichtig und welche weiteren Anmerkungen hast du?"), 105), collapse = "\n", sep = ""), 
+              daten = dat$Freitext, seite = i + sum(Seiten[1:3])+4, i = i)
+  }
   
-  # Zeile 1
-  plot.new()
-  text(-0.02, 0.2, paste(strwrap(paste("Welche weiteren Qualitätsaspekte sind dir bei der Arbeit des SAPV-Teams", SAPVteam, "wichtig und welche weiteren Anmerkungen hast du?"), 105), collapse = "\n", sep = ""), adj = c(0,NA), cex = 1.8, font = 2)
-  
-  # Zeile 2
-  plot(10, ylim = c(0,5), xlim = c(0,1), axes = FALSE)
-  text(-0.02, 5.1,
-       paste(strwrap(paste("- ", dat$Freitext[grep('[:alphanum:]', dat$Freitext)], sep = '\n'), 130), collapse = "\n", sep = ""), adj = c(0, 1), cex = 1.5)
-  
-  # Zeile 3
-  plot.new()
-  lines(c(-0.02,3), c(1,1))
-  text(1.04, 0.1, "Seite 5 von 5", adj = c(1,0))
-  text(-0.02, 0.1, Fussn, adj = c(0,0))
-  
+#   lmatr5 <- matrix(c(1,2,3), nrow = 3)
+#   layout(lmatr5, heights = lcm(c(2.5,16,0.5)))
+#   
+#   # Zeile 1
+#   plot.new()
+#   text(-0.02, 0.2, paste(strwrap(paste("Welche weiteren Qualitätsaspekte sind dir bei der Arbeit des SAPV-Teams", SAPVteam, "wichtig und welche weiteren Anmerkungen hast du?"), 105), collapse = "\n", sep = ""), adj = c(0,NA), cex = 1.8, font = 2)
+#   
+#   # Zeile 2
+#   plot(10, ylim = c(0,5), xlim = c(0,1), axes = FALSE)
+#   text(-0.02, 5,
+#        paste(Zeilen(dat$Freitext), collapse = "\n"), adj = c(0, 1), cex = 1.5)
+#   
+#   # Zeile 3
+#   plot.new()
+#   lines(c(-0.02,3), c(1,1))
+#   text(1.04, 0.1, "Seite 5 von 5", adj = c(1,0))
+#   text(-0.02, 0.1, Fussn, adj = c(0,0))
+#   
   
   ######## Erstellen abschließen ##########
   
   dev.off()
 }
 
-Auswertung(Eingabe)
+Auswertung(team = "PCT-Ost")
+
+
+##### Diese Unterfunktion teilt Freitext in gut druckbare Zeilen auf.
+# Zeilen <- function(x, länge = 100, ...){
+#   strwrap(
+#     unlist(
+#       strsplit(
+#         paste(unlist(
+#           strsplit(x, "-&&-")
+#         ), 
+#         collapse = "\\n\\n"), 
+#         "\\\\n")
+#     ), länge
+#   )  
+# }
+# 
+# plot.new()
+# text(0, 1, paste(Zeilen(dat[,37], 120), collapse = "\n"), adj = c(0,1), cex = 0.5)
+# length(Zeilen(dat[,37], 100))
